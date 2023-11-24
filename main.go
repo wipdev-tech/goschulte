ccadbe65eb1bd67abac2aa46048f1c4691dd65e5
wipdev-tech <wipdev.tech@gmail.com>
2023-11-24 22:45:16 +0200

    Initial server code and home template

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+    fs := http.FileServer(http.Dir("/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+    http.HandleFunc("/", handleHome)
+
+    fmt.Println("Server started and running at http://localhost:8080")
+    log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+    tmpl := template.Must(template.ParseFiles("templates/index.html"))
+    tmpl.Execute(w, 42)
+}
