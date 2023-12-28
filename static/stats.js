65d9ed3df65a2c30936099bc60b05d02a6f82667
wipdev-tech <wipdev.tech@gmail.com>
2023-12-28 11:37:51 +0200

    Simple display of mean completion times

--- static/stats.js
+++ static/stats.js
@@ -1,1 +1,23 @@
-console.log("Hello stats!")
+/**
+ * @typedef {number[]} numbers
+ */
+
+/**
+ * Calculates the mean of an array of numbers.
+ * @param {numbers} ar - an array of numbers
+ * @returns {number} Calculated mean.
+ */
+function mean(ar) {
+    const sum = ar.reduce((acc, e) => acc + e)
+    return sum / ar.length
+}
+
+const times = JSON.parse(localStorage.getItem("times"))
+
+for (size in times) {
+    const m = mean(times[size])
+    const liNew = document.createElement("li")
+    liNew.classList.add("text-lg")
+    liNew.innerHTML = `<b>${size}x${size}</b>: ${m}s`
+    document.getElementById("stats").append(liNew)
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./templates/*.html"],
+    content: ["./templates/*.html", "./static/*.js"],
     theme: {
         extend: {},
         fontFamily: {
